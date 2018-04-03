a6f76bd139d82c3af5992ff9e0b1e505330e2f9d
Stuart <stuart.chinery@gmail.com>
2018-04-03 12:08:21 +0100

    Added minitest-osx gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,13 +3,17 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "boxt_ruby_style_guide"
 
+# Make sure require "minitest/reporters" is before require "minitest/auto"
+require "minitest/reporters"
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  "test",
+  Minitest.backtrace_filter
+)
+
+# Needed by "minitest/osx" because it uses cattr_accessor
+# See: https://github.com/tombell/minitest-osx/issues/3
+require "active_support/all"
 require "minitest/autorun"
 require "minitest/fail_fast"
-require "minitest/reporters"
-
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
-Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-Minitest::Reporters.use!(
-  Minitest::Reporters::SpecReporter.new
-)
+require "minitest/osx"
