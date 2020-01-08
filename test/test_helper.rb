5cd9712ab050fc51826f1eb988ccd6cd49bf851b
Stuart Chinery <stuart.chinery@gmail.com>
2020-01-08 14:50:04 +0000

    Updated Minitest reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,7 @@ require "minitest/reporters"
 
 Minitest::Reporters.use!(
   [
-    Minitest::Reporters::SpecReporter.new,
+    Minitest::Reporters::DefaultReporter.new,
     Minitest::Reporters::MacosNotificationReporter.new(title: "BOXT Ruby Style Guide")
   ],
   "test",
