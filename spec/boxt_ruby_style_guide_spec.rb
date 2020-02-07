399c2331f477e6080c790e142b50aaaf380693c0
Stuart Chinery <stuart.chinery@gmail.com>
2020-02-07 13:32:16 +0000

    Amends

--- spec/boxt_ruby_style_guide_spec.rb
+++ spec/boxt_ruby_style_guide_spec.rb
@@ -3,7 +3,7 @@
 require("spec_helper")
 
 describe(::BoxtRubyStyleGuide) do
-  it("is a Module") { expect(::BoxtRubyStyleGuide).to(be_a(Module)) }
+  it("is a Module") { expect(described_class).to(be_a(Module)) }
 
   it("haves a version set") do
     expect(::BoxtRubyStyleGuide::VERSION).to(be_truthy)
