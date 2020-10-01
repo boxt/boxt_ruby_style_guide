f15cefcb497fe38c0005d7c20e61ecf69cbb5da7
Stuart Chinery <stuart.chinery@gmail.com>
2020-10-01 07:28:40 +0100

    Rubocop fixes for the Railtie spec

--- spec/boxt_ruby_style_guide/railtie_spec.rb
+++ spec/boxt_ruby_style_guide/railtie_spec.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require 'boxt_ruby_style_guide/railtie'
-require 'rake'
-RSpec.describe 'BoxtRubyStyleGuide::Ralitie' do
+require "boxt_ruby_style_guide/railtie"
+require "rake"
 
-  describe "rake_tasks hook" do
-    it "loads the rake tasks from lib/tasks" do
-      task_name = "lint:rubocop"
-      expect(Rake::Task).to_not be_task_defined(task_name)
+describe BoxtRubyStyleGuide::Railtie do
+  describe "rake_tasks Railtie hook" do
+    let(:task_name) { "lint:rubocop" }
+
+    it "does not load the rake task 'lint:rubocop' from lib/tasks by default" do
+      expect(Rake::Task).not_to be_task_defined(task_name)
+    end
+
+    it "loads the rake task 'lint:rubocop' from lib/tasks after the Rails hook" do
       simulate_railtie_init
       expect(Rake::Task).to be_task_defined(task_name)
     end
